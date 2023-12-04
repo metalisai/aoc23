29b19462f15ecb059fd6a070b09815d697c41ee2
Talis Tammearu <talis995@gmail.com>
2023-12-05 01:43:21 +0200

    like the cool kids!

--- day4_1.py
+++ day4_1.py
@@ -1,2 +1,1 @@
-import re
-print(sum(int(2**(len([x for x in m if x in w])-1)) for w, m in [[list(re.sub('\s+', ' ', x.strip()).split(' ')) for x in line.split(':')[1].split('|')] for line in open("4", "r").readlines()]))
+import re;print(sum(int(2**(len([x for x in m if x in w])-1))for w,m in[[list(re.sub(' +', ' ',x.strip()).split(' '))for x in l.split(':')[1].split('|')]for l in open("4", "r").readlines()]))
